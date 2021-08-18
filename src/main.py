34b8d1734edfac8bcae911e759eef720e3b36d93
Daniil Yarockii <yaroc.d@gmail.com>
2021-08-18 12:55:13 +0300

    app can starting now

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 import functools
 from functools import lru_cache
 
@@ -27,80 +26,27 @@ def send_error_data(func):
     return wrapper
 
 
-@g.my_app.callback("get_model_meta")
-@sly.timeit
-@send_error_data
-def get_model_meta(api: sly.Api, task_id, context, state, app_logger):
-    """
-    return: model meta
-    """
-    request_id = context["request_id"]
-    g.my_app.send_response(request_id, data=g.meta.to_json())
 
-
-@g.my_app.callback("get_session_info")
+@g.my_app.callback("ping")
 @sly.timeit
 @send_error_data
 def get_session_info(api: sly.Api, task_id, context, state, app_logger):
-    """
-    return: dict with info about model
-    """
-    model_data = functions.get_model_data()
-
-    info = {
-        "app": "FairMOT Serve",
-        "weights": g.remote_weights_path,
-        "device": g.device,
-        "session_id": task_id,
-        "model arch": model_data['arch'],
-        "model epoch": model_data['epoch'],
-        "heads": model_data['heads'],
-        "head_conv": model_data['head_conv']
-    }
-    request_id = context["request_id"]
-    g.my_app.send_response(request_id, data=info)
+    pass
 
 
-@g.my_app.callback("inference_video_id")  # to video, input_video_id[frame_range] -> output_annotation_for_video
+@g.my_app.callback("track")
 @sly.timeit
 @send_error_data
 def inference_video_id(api: sly.Api, task_id, context, state, app_logger):
-    app_logger.debug("Input data", extra={"state": state})
-    video_id = state["videoId"]
-    frames_range = state.get("framesRange", None)
-    conf_thres = state.get("confThres", 0)
-    is_preview = state.get("isPreview", False)
-
-    preview_video_url = None
-
-    annotations, preview_video_path = functions.process_video(video_id, frames_range, conf_thres, is_preview)
-    if is_preview:
-        file_info = functions.upload_video_to_sly(preview_video_path)
-        preview_video_url = file_info.full_storage_url
-
-    request_id = context["request_id"]
-    g.my_app.send_response(request_id, data={'ann': annotations.to_json(), 'preview_url': preview_video_url})
-
-
-@g.my_app.callback("inference_batch_ids")
-@sly.timeit
-@send_error_data
-def inference_batch_ids(api: sly.Api, task_id, context, state, app_logger):
-    raise NotImplementedError("Please contact tech support")
+    pass
 
 
 def main():
     sly.logger.info("Script arguments", extra={
         "context.teamId": g.team_id,
         "context.workspaceId": g.workspace_id,
-        "modal.state.slyFile": g.remote_weights_path,
         "device": g.device
     })
-
-    functions.download_model_and_configs()
-    functions.construct_model_meta()
-    functions.deploy_model()
-
     g.my_app.run()
 
 
